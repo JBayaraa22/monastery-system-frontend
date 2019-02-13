c4835bf462ca5cdb2dc3e3a87ea78558fb63762c
Bayarjargal Jargalsaikhan <gl.bayaraa@gmail.com>
2019-02-13 18:36:41 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin/admin.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
